e6afb30af7908f34eca4f2b9ba8f7eeabc7f547a
FavianTheBest <dev.favian@gmail.com>
2025-08-14 01:45:01 +0200

    Add crouch mechanic and fix fox hitbox detection
    - Implemented crouch action triggered by right mouse click (dxclick)
    - Added separate collider for crouch state (CollisionCrouch) and disabled standing collider (CollisionUp) while crouching
    - Fixed hitbox alignment to ensure collisions with enemies trigger 'hit' signal correctly
    - Updated animation logic to prevent re-triggering animations every frame

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,7 +17,7 @@ func _on_enemy_timer_timeout() -> void:
 	var enemy_instance = enemy_scene.instantiate()
 	enemy_instance.position = Vector2(152, 165)
 	if enemy_instance.name == "Eagle":
-		enemy_instance.position.y -= 40
+		enemy_instance.position.y -= 25
 	add_child(enemy_instance)
 	pass # Replace with function body.
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,19 +2,37 @@ extends CharacterBody2D
 
 
 const JUMP_VELOCITY = -300.0
+
 signal hit
+var is_crouching = false
+@onready var col_walk := $CollisionUp
+@onready var col_crouch := $CollisionCrouch
 
 func _physics_process(delta: float) -> void:		#repeat during the game execution
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	else:
-		$AnimatedSprite2D.play("run")
+	#else:
+		#$AnimatedSprite2D.play("run")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("click") and is_on_floor():
+	if Input.is_action_just_pressed("sxclick") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		$AnimatedSprite2D.play("jump")
+		
+	if Input.is_action_pressed("dxclick") and not is_crouching and is_on_floor():
+		_enter_crouch()
+	elif not Input.is_action_pressed("dxclick") and is_crouching and is_on_floor():
+		_exit_crouch()
+	
+	if is_on_floor():
+		if is_crouching:
+			$AnimatedSprite2D.play("down")
+		elif velocity.y >= 0.0:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("jump")
+
 
 	move_and_slide()
 
@@ -22,3 +40,13 @@ func _physics_process(delta: float) -> void:		#repeat during the game execution
 	if collision:
 		if collision.get_collider() is CharacterBody2D:
 			hit.emit()
+
+func _enter_crouch() -> void:
+	is_crouching = true
+	col_walk.disabled = true
+	col_crouch.disabled = false
+
+func _exit_crouch() -> void:
+	is_crouching = false
+	col_walk.disabled = false
+	col_crouch.disabled = true
